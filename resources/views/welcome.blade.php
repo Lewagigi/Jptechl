38f583d558fa8510e990802b44478ad863fedf05
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-26 12:57:18 +0100

    fififilopplmlmloplopl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,19 +121,13 @@
                 </button>
             </div>
 
-            <ul id="features" class="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 text-sm text-gray-700">
-                <li class="flex items-start gap-3">
+
+          
                     <span class="text-amber-700 font-bold"></span>
                     <h3  class="mt-4 text-gray-700"> Le financement permettra les rencontres entre les étudiants et les professionnels
                         en louant des salles pour organiser des moments d’échanges autour d’un café.
                         Permettre aussi de soutenir la plateforme.</h3>
-                </li>
 
-                <li class="flex items-start gap-3">
-                    <span class="text-amber-700 font-bold"></span>
-                    <!-- Ajouter ici le second point si nécessaire -->
-                </li>
-            </ul>
         </div>
 
 
