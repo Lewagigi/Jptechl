ed72f036a2c06f71a15d20b5d17f42a2d375ffa8
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-26 12:13:00 +0100

    fififilopplmlmloplopl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,7 +148,12 @@
         <div class="w-full mx-auto p-4 sm:p-6 bg-white rounded-2xl shadow-lg">
 
             <h3 class="text-2xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">
-                <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">ADHEREER / SOUTENIR</a>
+                <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">ADHEREER / SOUTENIR </a>
+                <button id="btn-login"
+                        class="px-4 py-2 bg-amber-700 text-white rounded-md shadow">
+                    <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">CLIQUEZ ICI </a>
+                </button>
+
             </h3>
 
             <p>Jeunes Prodiges Tech est une plate-forme d'accompagnent et de conseils des jeunes qui sont dans la Tech dans leur recherche d’alternances,
