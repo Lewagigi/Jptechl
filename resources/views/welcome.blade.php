fa8a2a9dcd9181cfd2cc976f2538864e1184c853
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-26 16:30:09 +0100

    fopopioiuopoik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
     <nav class="flex items-center justify-between w-full">
 
         <!-- VERSION DESKTOP -->
-        <div class="hidden md:flex gap-4 items-center ml-auto">
+        <div class="hidden md:flex gap-4 items-center justify-end mr-10">
             <a href="#features" class="text-sm hover:underline"></a>
             <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-sm hover:underline"></a>
 
@@ -42,6 +42,7 @@
                 Créer un compte
             </button>
         </div>
+
 
 
         <!-- VERSION MOBILE -->
