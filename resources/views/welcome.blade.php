2eeb6d4673ef599bbcca86cc10412a5a5a086dba
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-26 12:24:45 +0100

    fififilopplmlmloplopl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,7 +148,7 @@
         <div class="w-full mx-auto p-4 sm:p-6 bg-white rounded-2xl shadow-lg">
 
             <h3 class="text-2xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">
-                <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">ADHEREER</a>
+                <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-amber-800 leading-tight">ADHESION À LA PLATE-FORME</a>
                 <button id="btn-login"
                         class="px-4 py-2 bg-amber-700 text-white rounded-md shadow">
                     <a href="https://donorbox.org/jeunes-prodiges-tech" class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-black-800 leading-tight">CLIQUEZ ICI </a>
