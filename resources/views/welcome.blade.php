dda6ba4c8100a596bde9273a3300d0d017ec24d9
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-26 18:43:37 +0100

    fopopioiuopoik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -249,8 +249,9 @@ NOS MISSIONS
 
 <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
     <div class="bg-white rounded-lg shadow p-6 flex flex-col">
-        <div class="h-40 bg-gray-200 rounded-md mb-4"></div>
-        <img src="/img/Design sans titre.png" alt="" class="size-8" />
+        <div class="h-40 bg-gray-200 rounded-md mb-4"> <img src="/img/Design sans titre.png" alt="" class="size-8" />
+        </div>
+
         <h3 class="text-lg font-semibold mb-2">Titre de la carte</h3>
         <p class="text-sm text-gray-600 flex-grow">Description ou contenu de la carte. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <button class="mt-4 px-4 py-2 bg-amber-700 text-white rounded-md shadow">Action</button>
